df3d26436071f545b9a87caeab74e47eb2654cbe
Affaq-Ahmed <affaqahmed165@gmail.com>
2024-07-01 21:19:39 +0500

    add Phone Component

--- src/components/Phone.tsx
+++ src/components/Phone.tsx
@@ -21,10 +21,22 @@ const Phone = ({
 			{...props}
 		>
 			<img
+				src={
+					dark
+						? '/phone-template-dark-edges.png'
+						: '/phone-template-white-edges.png'
+				}
 				className='pointer-events-none z-50 select-none'
-				src={dark ? '/phone-template-dark-edges.png' : "/phone-template-light-edges.png"}
 				alt='phone image'
 			/>
+
+			<div className='absolute -z-10 inset-0'>
+				<img
+					src={imgSrc}
+					alt='overlaying phone image'
+					className='object-cover'
+				/>
+			</div>
 		</div>
 	);
 };
